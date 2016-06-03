58a19b4f98f722fae0ca803fd41ee8954472da32
2Cubed <2CubedBot@gmail.com>
2016-06-02 22:01:42 -0400

    P006: Fix docstring formatting

--- euler/p006.py
+++ euler/p006.py
@@ -7,6 +7,7 @@ MAXIMUM = 100
 
 def compute(maximum=MAXIMUM):
     """Compute the difference between the square of the sum and the sum of the
-    squares of the first `maximum` natural numbers."""
+    squares of the first `maximum` natural numbers.
+    """
 
     return sum(range(1, maximum+1))**2 - sum(n**2 for n in range(1, maximum+1))
